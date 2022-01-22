8af07bc576983999cd148b766cf738d56327c0ce
V1n1c1uzDev <viniciusdesigner0@gmail.com>
2022-01-22 20:11:37 -0300

    #refactor: Adicionar rota inicial da api

--- src/server.js
+++ src/server.js
@@ -3,6 +3,10 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+app.get('/', (req, res) => {
+  res.json({ message: 'Bem vindo a Template-Express API' })
+})
+
 app.listen(PORT, () => {
   console.log(`Estou rodando na porta ${PORT}`);
 })
